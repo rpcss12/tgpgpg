1188b5d1c99abd4dfec843dca189cf48974e97ca
rpcss12 <85547887+rpcss12@users.noreply.github.com>
2021-06-08 16:49:26 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,31 @@
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    count = randint(1, 3)
+})
+basic.forever(function () {
+    if (count == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (count == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    } else if (count == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
